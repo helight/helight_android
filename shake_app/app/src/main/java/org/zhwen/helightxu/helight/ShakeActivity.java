7227cb622c3e424626269039a48e205848357a95
unknown <helightxu@HELIGHTXU-PC8.tencent.com>
2014-11-08 14:29:19 +0800

    add ui_test

--- shake_app/app/src/main/java/org/zhwen/helightxu/helight/ShakeActivity.java
+++ shake_app/app/src/main/java/org/zhwen/helightxu/helight/ShakeActivity.java
@@ -1,6 +1,7 @@
 package org.zhwen.helightxu.helight;
 
 import android.app.Activity;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
@@ -19,12 +20,9 @@ import org.zhwen.helightxu.helight.ShakeListener.OnShakeListener;
 public class ShakeActivity extends Activity {
     ShakeListener mShakeListener = null;
     Vibrator mVibrator;
-    private RelativeLayout mImgUp;
-    private RelativeLayout mImgDn;
-    private RelativeLayout mTitle;
-
-    private SlidingDrawer mDrawer;
-    private Button mDrawerBtn;
+    DiceSurfaceView view;
+    Dice dice;
+    Canvas canvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,50 +32,30 @@ public class ShakeActivity extends Activity {
         setContentView(R.layout.activity_shake);
 
         mVibrator = (Vibrator)getApplication().getSystemService(VIBRATOR_SERVICE);
-        mImgUp = (RelativeLayout) findViewById(R.id.shakeImgUp);
-        mImgDn = (RelativeLayout) findViewById(R.id.shakeImgDown);
-        mTitle = (RelativeLayout) findViewById(R.id.shake_title_bar);
 
-        mDrawer = (SlidingDrawer) findViewById(R.id.slidingDrawer1);
-        mDrawerBtn = (Button) findViewById(R.id.handle);
-        mDrawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener()
-        {	public void onDrawerOpened()
-            {
-                mDrawerBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.shake_report_dragger_down));
-                TranslateAnimation titleup = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,-1.0f);
-                titleup.setDuration(200);
-                titleup.setFillAfter(true);
-                mTitle.startAnimation(titleup);
-            }
-        });
-		 /* 设定SlidingDrawer被关闭的事件处理 */
-        mDrawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener()
-        {	public void onDrawerClosed()
-            {
-                mDrawerBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.shake_report_dragger_up));
-                TranslateAnimation titledn = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,-1.0f,Animation.RELATIVE_TO_SELF,0f);
-                titledn.setDuration(200);
-                titledn.setFillAfter(false);
-                mTitle.startAnimation(titledn);
-            }
-        });
+        view = (DiceSurfaceView)findViewById(R.id.view);
+        dice = new Dice(getResources());
+        canvas = new Canvas();
+        view.i = 1;
+        view.isRun = true;
 
         mShakeListener = new ShakeListener(this);
         mShakeListener.setOnShakeListener(new OnShakeListener() {
             public void onShake() {
                 //Toast.makeText(getApplicationContext(), "抱歉，暂时没有找到在同一时刻摇一摇的人。\n再试一次吧！", Toast.LENGTH_SHORT).show();
-                startAnim();  //开始 摇一摇手掌动画
+                // startAnim();  //开始 摇一摇手掌动画
                 mShakeListener.stop();
                 startVibrato(); //开始 震动
                 new Handler().postDelayed(new Runnable(){
                     @Override
                     public void run(){
                         //Toast.makeText(getApplicationContext(), "抱歉，暂时没有找到\n在同一时刻摇一摇的人。\n再试一次吧！", 500).setGravity(Gravity.CENTER,0,0).show();
-                        Toast mtoast;
-                        mtoast = Toast.makeText(getApplicationContext(),
-                                "抱歉，暂时没有找到\n在同一时刻摇一摇的人。\n再试一次吧！", 10);
-                        //mtoast.setGravity(Gravity.CENTER, 0, 0);
-                        mtoast.show();
+                        // Toast mtoast;
+                        // mtoast = Toast.makeText(getApplicationContext(), "抱歉，暂时没有找到\n在同一时刻摇一摇的人。\n再试一次吧！", 10);
+                        // mtoast.setGravity(Gravity.CENTER, 0, 0);
+                        // mtoast.show();
+                        // view.isRun = true;
+
                         mVibrator.cancel();
                         mShakeListener.start();
                     }
@@ -86,27 +64,6 @@ public class ShakeActivity extends Activity {
         });
     }
 
-    public void startAnim () {   //定义摇一摇动画动画
-        AnimationSet animup = new AnimationSet(true);
-        TranslateAnimation mytranslateanimup0 = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,-0.5f);
-        mytranslateanimup0.setDuration(1000);
-        TranslateAnimation mytranslateanimup1 = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,+0.5f);
-        mytranslateanimup1.setDuration(1000);
-        mytranslateanimup1.setStartOffset(1000);
-        animup.addAnimation(mytranslateanimup0);
-        animup.addAnimation(mytranslateanimup1);
-        mImgUp.startAnimation(animup);
-
-        AnimationSet animdn = new AnimationSet(true);
-        TranslateAnimation mytranslateanimdn0 = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,+0.5f);
-        mytranslateanimdn0.setDuration(1000);
-        TranslateAnimation mytranslateanimdn1 = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,-0.5f);
-        mytranslateanimdn1.setDuration(1000);
-        mytranslateanimdn1.setStartOffset(1000);
-        animdn.addAnimation(mytranslateanimdn0);
-        animdn.addAnimation(mytranslateanimdn1);
-        mImgDn.startAnimation(animdn);
-    }
     public void startVibrato(){		//定义震动
         mVibrator.vibrate( new long[]{500,200,500,200}, -1); //第一个｛｝里面是节奏数组， 第二个参数是重复次数，-1为不重复，非-1俄日从pattern的指定下标开始重复
     }
@@ -114,9 +71,7 @@ public class ShakeActivity extends Activity {
     public void shake_activity_back(View v) {     //标题栏 返回按钮
         this.finish();
     }
-    public void linshi(View v) {     //标题栏
-        startAnim();
-    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
