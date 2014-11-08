7713e6e68483a38b99c73f804623218fb705d399
unknown <helightxu@HELIGHTXU-PC8.tencent.com>
2014-11-08 17:32:33 +0800

    add fragment tab

--- ui_test/src/org/zhwen/helight_ui/StartActivity.java
+++ ui_test/src/org/zhwen/helight_ui/StartActivity.java
@@ -1,7 +1,5 @@
 package org.zhwen.helight_ui;
 
-import com.anjoyo.liuxiaowei.FrameActivity;
-import com.anjoyo.liuxiaowei.StartActivity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,27 +7,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class StartActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-		
-        super.onCreate(savedInstanceState);
-        // requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //隐藏标题
         // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
         //         WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
-        // setContentView(R.layout.activity_main);
+
         setContentView(R.layout.activity_start);
         
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent intent = new Intent(StartActivity.this,
-        				FrameActivity.class);
+                Intent intent = new Intent(StartActivity.this, TabActivity.class);
         		startActivity(intent);
         		StartActivity.this.finish();
             }
